65941800ac99e51ae08ff657056cc50bfe87327b
Tomas Aparicio <tomas@aparicio.me>
2013-11-17 04:44:08 +0100

    Updating provision.bat script

--- environment/stack/scripts/provision.bat
+++ environment/stack/scripts/provision.bat
@@ -1,36 +1,18 @@
 @ECHO OFF
-
-IF EXIST "%~dp0..\stack\node\node_modules\bower-auth" GOTO END
-
-CALL "%~dp0config.bat"
-
-:: package versions
-SET yo=~1.0.4
-SET bower=~1.2.7
-SET grunt=~0.1.9
+:: do not fun this script manually
 
 CLS 
 ECHO.
-ECHO  -----------------------------------------------------
-ECHO   Welcome to the Bankia Front environment provisioner
-ECHO  -----------------------------------------------------
+ECHO %YAWDS_CONF_GENERAL_NAME% provisioner
 ECHO.
-ECHO This process will install the required packages in the system
+ECHO This process will install the selected packages in the system
 ECHO Note that this process may take some minutes...
 ECHO.
 
-IF NOT DEFINED USER (
-	SET /P USER=Enter your Bankia username: 
-)
-IF NOT DEFINED PASSWORD (
-	SET /P PASSWORD=Enter your password: 
-	ECHO.
-)
-
-ECHO.
 ECHO Installing packages...
 ECHO.
 
+:: todo
 :: install Node packages
 CALL npm install -g yo@%yo%
 CALL npm install -g bower@%bower%
