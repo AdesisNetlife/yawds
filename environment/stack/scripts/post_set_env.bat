b11ad2bbde855592741c75cc84f37607d1aecf67
Tomas Aparicio <tomas@aparicio.me>
2013-11-21 00:27:29 +0100

    adding post_set_env script

--- environment/stack/scripts/post_set_env.bat
+++ environment/stack/scripts/post_set_env.bat
@@ -5,12 +5,12 @@ IF [%YAWDS_PKG_PROVISION_ENABLED%]==[0] GOTO END
 
 :: auto discover binaries directories for aditional packages
 :: useful to automatically add them to be path accesible
-IF EXIST "%YAWDS_HOME%\packages" (
+IF EXIST "%YAWDS_HOME%\%YAWDS_PKG_INSTALL_DIR%" (
 	SET CWD=%CD%
-	CD "%YAWDS_HOME%\packages"
+	CD "%YAWDS_HOME%\%YAWDS_PKG_INSTALL_DIR%"
 	FOR /D %%i IN (*.*) DO (
 		IF NOT [%%i]==[ruby] (
-			IF EXIST "%YAWDS_HOME%\packages\%%i\bin" (
+			IF EXIST "%YAWDS_HOME%\%YAWDS_PKG_INSTALL_DIR%\%%i\bin" (
 				SET PATH=%%YAWDS_HOME%\packages\%%i\bin%;%PATH%
 			)
 		)
